6eebc8d3422860b39ff640448932ef86f5590d91
chamegashi <msymsy0693wrb@gmail.com>
2021-10-24 14:40:51 +0900

    add lint range 'components'

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,16 +1,21 @@
-import Image from 'next/image'
-import { VFC } from 'react'
+import Image from "next/image";
+import { VFC } from "react";
 
 interface Props {
-  text: string
+  text: string;
 }
 
 const Card: VFC<Props> = ({ text }) => {
-  return(
+  return (
     <div className="p-3 m-3 w-80 text-white rounded border border-white">
       <div className="flex">
         <div className="relative w-14 h-14">
-          <Image className="rounded-full" src="/pro_exam1.jpg" layout="fill" objectFit="contain" />
+          <Image
+            className="rounded-full"
+            src="/pro_exam1.jpg"
+            layout="fill"
+            objectFit="contain"
+          />
         </div>
         <div className="ml-2">
           <p className="text-lg">username</p>
@@ -19,10 +24,10 @@ const Card: VFC<Props> = ({ text }) => {
       </div>
 
       <div className="relative mt-2 h-80 rounded-3xl">
-        <Image src="/exam1.jpg" layout="fill" objectFit="contain"/>
+        <Image src="/exam1.jpg" layout="fill" objectFit="contain" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
